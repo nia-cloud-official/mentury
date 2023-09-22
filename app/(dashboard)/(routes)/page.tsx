25b437f4469cab506d37728db282a83dbf0a5d26
leoarayav <leandro.araya08@inacapmail.cl>
2023-09-22 18:11:04 -0300

    refactoring the colors

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
     return (
         <div className="mt-40 antialiased">
             <div className="mt-40 antialiased">
-                <h1 className="text-5xl font-extrabold tracking-tight text-center text-orange-500">
+                <h1 className="text-5xl font-extrabold tracking-tight text-center text-neutral-500">
                     Mentury
                 </h1>
                 <p className="text-center text-neutral-600 mt-2 text-lg">
