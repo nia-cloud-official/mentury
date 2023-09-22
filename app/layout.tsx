b5acd0978910244f1197ed9ab1ac6882ec047a6c
leoarayav <leandro.araya08@inacapmail.cl>
2023-09-22 18:10:41 -0300

    clerk provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/providers/theme'
 import { ToastProvider } from '@/providers/toast'
+import { ClerkProvider } from "@clerk/nextjs"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,13 +18,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ToastProvider />
-        <ThemeProvider enableSystem attribute='class' defaultTheme='dark'>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider enableSystem attribute='class' defaultTheme='dark'>
+            <ToastProvider />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
