217401330a68360777c727faf26a87a583ebd34a
leoarayav <leandro.araya08@inacapmail.cl>
2023-09-22 16:20:30 -0300

    components and layout who used next-auth modified

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,7 @@
 import { NavbarRoutes } from "./navbar-routes"
 import { MobileSidebar } from "./sidebar/mobile-sidebar"
-import { getAuthSession } from "@/lib/auth"
-import { UserAvatar } from "./user/user-avatar"
 
 export const Navbar = async () => {
-    const session = await getAuthSession()
     return (
         <div className="p-4 h-full flex items-center backdrop-blur-md shadow-sm border-b border-neutral-900">
             <MobileSidebar />
