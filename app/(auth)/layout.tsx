e12d789980d99a9b94593050a3a77ededf93e9cc
leoarayav <leandro.araya08@inacapmail.cl>
2023-09-23 00:42:20 -0300

    better auth routes display

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-40">
             {children}
         </div>
     )
